dc3f573585da5a6702c158137defc582b597d82d
Nicholai Nissen <nicholainissen@disroot.org>
2025-05-10 11:50:45 +0200

    feat: add insert site type

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/d1'
-import { Context } from 'effect'
+import { Context, } from 'effect'
 import * as schema from '../database/schema'
 
 export { sql, eq, and, or } from 'drizzle-orm'
@@ -20,4 +20,5 @@ export class DrizzleService extends Context.Tag('DrizzleService')<
   static live = () => useDrizzle()
 }
 
-export type Site = typeof schema.sites.$inferSelect
+export type SiteSelect = typeof schema.sites.$inferSelect
+export type SiteInsert = typeof schema.sites.$inferInsert
