4a8bf8cbc50452d658ff04c39194655eb2aa730c
Nicholai Nissen <nicholainissen@disroot.org>
2025-05-11 08:45:10 +0200

    feat!: Use Effect native Drizzle implementation

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,11 +1,16 @@
 import { drizzle } from 'drizzle-orm/d1'
-import { Context, } from 'effect'
+import { Context, Effect, Layer, } from 'effect'
 import * as schema from '../database/schema'
+import { layer as SqliteDrizzleLayer } from '@effect/sql-drizzle/Sqlite'
+import * as D1Client from '@effect/sql-d1/D1Client'
 
 export { sql, eq, and, or } from 'drizzle-orm'
 
 export const tables = schema
 
+/**
+ * @deprecated use SqliteDrizzle service imported from @effect/sql-drizzle/Sqlite
+ */
 export function useDrizzle () {
   return {
     db: drizzle(hubDatabase(), { schema }),
@@ -13,6 +18,9 @@ export function useDrizzle () {
   }
 }
 
+/**
+ * @deprecated use SqliteDrizzle service imported from @effect/sql-drizzle/Sqlite
+ */
 export class DrizzleService extends Context.Tag('DrizzleService')<
   DrizzleService,
   ReturnType<typeof useDrizzle>
@@ -20,5 +28,16 @@ export class DrizzleService extends Context.Tag('DrizzleService')<
   static live = () => useDrizzle()
 }
 
+const D1Live = D1Client.layer({
+  // @ts-expect-error There is a version mismatch between the two libs, but the missing feature is a deprecated fn.
+  db: hubDatabase()
+})
+
+export const DrizzleLive = SqliteDrizzleLayer.pipe(
+  Layer.provide(D1Live)
+)
+
 export type SiteSelect = typeof schema.sites.$inferSelect
 export type SiteInsert = typeof schema.sites.$inferInsert
+
+
