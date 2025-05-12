923335b61042fad9583b56c5390e9bd4986d12fc
Nicholai Nissen <nicholainissen@disroot.org>
2025-05-12 10:10:31 +0200

    feat: add SiteService list functionality

--- features/sites/service.ts
+++ features/sites/service.ts
@@ -1,5 +1,15 @@
-import { Context } from "effect";
+import { Effect } from 'effect'
+import { SqliteDrizzle } from '@effect/sql-drizzle/Sqlite'
 
-export class SiteService extends Context.Tag('SiteService')<SiteService, {
-  createNewSite: (url: string) => Site
-}>() {}
+export class SiteService extends Effect.Service<SiteService>()('SiteService', {
+  effect: Effect.gen(function* () {
+    const db = yield* SqliteDrizzle
+
+    const list = Effect.fn('SiteService#list')(function* () {
+      return yield* db.select().from(tables.sites)
+    })
+
+    return { list } as const
+  }),
+  accessors: true
+}) {}
