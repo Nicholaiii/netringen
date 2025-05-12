e4e57e8688c86781fa688b7853c5654bbf4002a3
Nicholai Nissen <nicholainissen@disroot.org>
2025-05-12 10:45:38 +0200

    style: unbreak line

--- server/api/sites/index.get.ts
+++ server/api/sites/index.get.ts
@@ -3,8 +3,7 @@ import { Site } from '~~/features/sites/model'
 import { SiteService } from '~~/features/sites/service'
 
 export default defineEventHandler(async () => {
-  const pipeline =
-    SiteService.list().pipe(
+  const pipeline = SiteService.list().pipe(
     Effect.flatMap(Schema.decode(Schema.Array(Site))),
     Effect.provide(SiteService.Default),
   )
