aa9096050ee4528671a855bb71b4f06a1e585f1b
Nicholai Nissen <nicholainissen@disroot.org>
2025-05-12 10:08:56 +0200

    feat: add Site model

---
+++ features/sites/model.ts
@@ -0,0 +1,8 @@
+import { Schema } from 'effect'
+
+export class Site extends Schema.Class<Site>('Site')(Schema.Struct({
+  id: Schema.Number,
+  name: Schema.String,
+  url: Schema.String,
+  integrated: Schema.Boolean
+}).annotations({ title: 'Webring Site' })) {}
