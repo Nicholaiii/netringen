dde9ef68f22b5fd4ea14462ca986a5d262c5a3ba
Nicholai Nissen <nicholainissen@disroot.org>
2025-05-13 13:55:38 +0200

    feat: add url parsing service

---
+++ features/sites/parsing/url.ts
@@ -0,0 +1,26 @@
+import { Url } from '@effect/platform'
+import { Effect, Either, pipe } from 'effect'
+import { IllegalArgumentException } from 'effect/Cause'
+
+export class URLParsingService extends Effect.Service<URLParsingService>()('URLParsingService', {
+  sync: () => {
+    const isFQDN = (hostname: string): boolean => {
+      const pattern = /^(?!:\/\/)(?=.{1,255}$)(?:.{1,63}\.){1,127}(?!\d*$)[a-z0-9-]+\.?$/im
+      return pattern.test(hostname)
+    }
+
+    const parse = (maybeUrl: string) => pipe(
+      Url.fromString(maybeUrl),
+      Either.filterOrLeft(
+        url => isFQDN(url.hostname),
+        () => new IllegalArgumentException('Absolute URL required'),
+      ),
+      Either.map(Url.setProtocol('https:')),
+    )
+
+    return {
+      parse,
+    } as const
+  },
+  accessors: true,
+}) {}
