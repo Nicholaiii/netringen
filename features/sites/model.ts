a9d9d69f6a794262a2fbd228a0e9ad42020045ec
Nicholai Nissen <nicholainissen@disroot.org>
2025-05-18 17:54:49 +0200

    feat: add insert model

--- features/sites/model.ts
+++ features/sites/model.ts
@@ -1,8 +1,13 @@
 import { Schema } from 'effect'
 
-export class Site extends Schema.Class<Site>('Site')(Schema.Struct({
+/** @internal */
+const SiteSchema = Schema.Struct({
   id: Schema.Number,
   name: Schema.String,
   url: Schema.String,
   integrated: Schema.Boolean,
-}).annotations({ title: 'Webring Site' })) {}
+}).annotations({ title: 'Webring Site' })
+
+export class Site extends Schema.Class<Site>('Site')(SiteSchema) {}
+
+export const SiteInsert = SiteSchema.omit('id', 'integrated').annotations({ title: 'Unsubmitted Webring Site' })
