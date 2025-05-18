29a2beb33d81649266087055f461e06af9d52bbf
ngajhede <ngajhede@gmail.com>
2025-05-18 19:53:54 +0200

    remove "rules" add just code of conduct tldr and links

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -27,27 +27,36 @@
       <div class="register">
         <Register />
       </div>
-      <h4>
-        {{ $t("join.requirements") }}
-      </h4>
-      <ul>
-        <li>
-          {{ $t("join.requirements1") }}
-        </li>
-        <li>
-          {{ $t("join.requirements2") }}
-        </li>
-        <li>
-          {{ $t("join.requirements3") }}
-        </li>
-      </ul>
 
       <h3>
         {{ $t("join.code_of_conduct") }}
       </h3>
       <p>
-        {{ $t("join.code_of_conduct_explanation") }}
+        {{ $t("join.code_of_conduct_reference") }}
+        <a
+          target="_blank"
+          href="https://www.contributor-covenant.org/version/2/1/code_of_conduct/"
+        >
+          {{ $t("join.contributor_covenant_link") }}</a
+        >
+        {{ $t("join.and_the") }}
+        <a target="_blank" href="https://yesterweb.org/#manifesto">
+          {{ $t("join.yesterweb_manifesto_link") }}</a
+        >.
       </p>
+      <p>{{ $t("join.code_of_conduct_tldr") }}</p>
+      <ul>
+        <li>
+          {{ $t("join.conduct_inclusive") }}
+        </li>
+        <li>
+          {{ $t("join.conduct_responsibility") }}
+        </li>
+        <li>
+          {{ $t("join.conduct_wellbeing") }}
+        </li>
+        <li>{{ $t("join.conduct_preserve") }}</li>
+      </ul>
     </section>
     <Separator />
   </div>
