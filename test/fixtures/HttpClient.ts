2e874e4651d46da03bcc5cfe14fc1a667adb87a7
Nicholai Nissen <nicholainissen@disroot.org>
2025-05-18 17:55:25 +0200

    test: add Example Site html

--- test/fixtures/HttpClient.ts
+++ test/fixtures/HttpClient.ts
@@ -1,7 +1,20 @@
 import { HttpClient, HttpClientResponse } from '@effect/platform'
 import { Effect, Layer } from 'effect'
 
-export const mockClientWithResponse = (response: Response) => Layer.succeed(HttpClient.HttpClient, HttpClient.make(req =>
+export const IdealSite = `
+<!doctype html>
+<html lang=en>
+  <head>
+    <meta charset=utf-8>
+    <title>komputer.club</title>
+  </head>
+  <body>
+    <p>Nothing here.</p>
+  </body>
+</html>
+`
+
+export const mockClientWithResponse = (response: Response = new Response(IdealSite)) => Layer.succeed(HttpClient.HttpClient, HttpClient.make(req =>
   Effect.succeed(
     HttpClientResponse.fromWeb(
       req,
